858a74629392aa872deaec93345387366ce1f123
cdevries546 <cdevries@comcast.net>
2018-07-16 17:28:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 $(document).ready(function searchRepositories (){
-
-}
+$.ajax({
+  type: "GET",
+  url: "https://api.meetup.com/2/cities",
+  success: function(data){
+    $('text').text(JSON.stringify(data));
+    
+  }, 
+  dataType: 'jsonp',
+});
+});
